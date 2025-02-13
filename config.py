58bfd0c62029f3153a1281234082ac3512dc5348
Boffob <super.lol.lol2015@mail.ru>
2025-02-13 21:56:51 +0300

    My first Commit

--- config.py
+++ config.py
@@ -2,7 +2,9 @@
 # Constants for DBF directory and SQLite database path
 
 # Example path to the directory containing your DBF files
-dbf_directory = "/path/to/your/dbf/files"
+#dbf_directory = "/path/to/your/dbf/files"
+dbf_directory = "A:\\GitHub\\dbf_to_sqlite\\DB"
 
 # Example path to your output SQLite database file
-database_path = "/path/to/your/output/database.db"
+database_path = "A:\\GitHub\\dbf_to_sqlite\\DB\\database.db"
+#database_path = "/path/to/your/output/database.db"
